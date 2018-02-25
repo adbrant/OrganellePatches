37995c5e7b2ee166572918868048458c585217ed
adbrant <alex.d.brant@gmail.com>
2018-02-25 14:23:04 -0500

    new stuff

---
+++ marimba/samples.bat
@@ -0,0 +1,48 @@
+mkdir   1
+copy    1.wav   .\1\1.wav
+mkdir   2
+copy    2.wav   .\2\2.wav
+mkdir   3
+copy    3.wav   .\3\3.wav
+mkdir   4
+copy    4.wav   .\4\4.wav
+mkdir   5
+copy    5.wav   .\5\5.wav
+mkdir   6
+copy    6.wav   .\6\7.wav
+mkdir   7
+copy    7.wav   .\7\7.wav
+mkdir   8
+copy    8.wav   .\8\8.wav
+mkdir   9
+copy    9.wav   .\9\9.wav
+mkdir   10
+copy    10.wav   .\10\10.wav
+mkdir   11
+copy    11.wav   .\11\11.wav
+mkdir   12
+copy    12.wav   .\12\12.wav
+mkdir   13
+copy    13.wav   .\13\13.wav
+mkdir   14
+copy    14.wav   .\14\14.wav
+mkdir   15
+copy    15.wav   .\15\15.wav
+mkdir   16
+copy    16.wav   .\16\16.wav
+mkdir   17
+copy    17.wav   .\17\17.wav
+mkdir   18
+copy    18.wav   .\18\18.wav
+mkdir   19
+copy    19.wav   .\19\19.wav
+mkdir   20
+copy    20.wav   .\20\20.wav
+mkdir   21
+copy    21.wav   .\21\21.wav
+mkdir   22
+copy    22.wav   .\22\22.wav
+mkdir   23
+copy    23.wav   .\23\23.wav
+mkdir   24
+copy    24.wav   .\24\24.wav
